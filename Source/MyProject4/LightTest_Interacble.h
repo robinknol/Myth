62efcedb36fdec7708322f8a5ae588fbb3c2f595
Miguel-Heijmans <58071327+Miguel-Heijmans@users.noreply.github.com>
2021-06-15 09:34:01 +0200

    wf
    cs

---
+++ Source/MyProject4/LightTest_Interacble.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Master_Interacble.h"
+#include "LightTest_Interacble.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYPROJECT4_API ALightTest_Interacble : public AMaster_Interacble
+{
+	GENERATED_BODY()
+	
+};
